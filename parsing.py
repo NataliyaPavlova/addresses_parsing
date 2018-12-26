5574585d4a58266cfc0036fc07d1c162db19037d
NataliyaPavlova <nataliya.s.pavlova@gmail.com>
2018-12-26 16:16:24 +0300

    correct few bugs

--- parsing.py
+++ parsing.py
@@ -21,15 +21,10 @@
                127.0.0.1 --> {"protocol": "http", "ip": "127.0.0.1", "rel_url": "/"}
                http://xn--90adear.xn--p1ai/ --> {'protocol': 'http', 'domain': 'гибдд.рф', 'rel_url': '/'}
 '''
-import pprint
 import requests
 import re
 import json
 
-# web-page ->  download (make a list of addresses)
-# addresses -> parsing
-#           -> make a list of dicts 
-# file -> jsonify
 
 class WebPage():
 
@@ -37,6 +32,7 @@ class WebPage():
         self.url = url
     
     def download(self):
+        ''' download text from web-page and save as a list of addresses '''
         r = requests.get(self.url)
         self.addrs_lst = str(r.content).split('\\n')
 
@@ -44,29 +40,37 @@ class WebPage():
 class Adresses(WebPage):
 
     def check_punycode(self, string):
-        # check if domain is punycode 
+        ''' check if domain is punycode and decode it '''
         if re.search('(\A)xn--', string): 
             string = string.encode().decode('idna') 
         return string
 
-    def parsing(self):
+    def parse(self):
+        ''' iterate through the list of addresses and parse it to dicts'''
         self.lst=[]
         for string in self.addrs_lst:
             if not re.search('(\A)http', string): 
-                print('Wrong string is {}'.format(string))
-                continue
-            protocol = string.split('://')[0]
-            dct = {'protocol' : protocol}
-            part1 = re.split('/', string.split('://')[1], 1)[0]
-            rel_url = '/' + re.split('/', string.split('://')[1], 1)[1] #if re.split('/', string.split('://')[0], 1)[1] else '/'
+                string = string.strip('b\"').strip('\"')
+                if not string:
+                    continue
+            #if address doesn't start with 'http': default protocol is 'http'
+            if len(string.split('://'))==1:
+                protocol = 'http'
+                part1 = string.split('://')[0]                 
+            else:
+                protocol = string.split('://')[0]
+                part1, rel_url = re.split('/', string.split('://')[1], 1)
 
+            dct = {'protocol' : protocol}
+            
             #check if IP (do not validate IP address, only simple check)
             if re.match('^[0-9\.]*$', part1):
                 dct['ip'] = part1
             else: 
                 domain = self.check_punycode(part1)
                 dct['domain'] = domain
-            dct['rel_url'] = rel_url
+            
+            dct['rel_url'] = '/' + rel_url
             self.lst.append(dct)
         
       
@@ -77,14 +81,15 @@ class Answer(Adresses, WebPage):
         self.filename = filename
 
     def jsonify(self):
+        ''' save the list of dicts to the given file'''
         with open(self.filename, 'w') as outfile:
             json.dump(self.lst, outfile)
 
 
 url = 'https://openphish.com/feed.txt'
-filename = 'result.json'
+filename = 'result.json' #for saving the result
 
 answer = Answer(url, filename)
 answer.download()
-answer.parsing()
+answer.parse()
 answer.jsonify()
